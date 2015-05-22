5d8766b0950c91e62541bb9ec4ac4d6b4534b085
Linus Liang <linus.liang@nyu.edu>
2015-05-22 10:39:35 -0400

    updated batch_controller.rb to handle different batch folder requests

--- app/controllers/api/v1/batch_controller.rb
+++ app/controllers/api/v1/batch_controller.rb
@@ -1,14 +1,15 @@
 class Api::V1::BatchController < Api::ApiController
   include ActionController::MimeResponds
   respond_to :json
+
   def index
     @array = []
-    i = 1
     if Tops.count > 1
       if params[:batch_folder] != nil
-        while i < 19
-          @array << Tops.where(number: i)
-          i+=1
+        for i in (1..18)
+          # check to see if it's of the correct batch folder
+          @array.push(findClothesWithBatchFolder(i,params[:batch_folder]))
+          # break
         end
         render json: @array
       else
@@ -20,4 +21,20 @@ class Api::V1::BatchController < Api::ApiController
              :json=>{:message=>"Shit there aren't any batches in here"}
     end
   end
+
+  # returns an array of tops with batch_folder given a param
+  # to search in sql db
+  def findClothesWithBatchFolder(num,batch_folder)
+    clothes =[]
+    Tops.where(number: num).each do |top|
+      top.batch_information.each do |batch_type|
+        if batch_type.include?(batch_folder)
+          clothes.push(top)
+          break
+        end
+      end
+    end
+    return clothes
+  end
+
 end
