df6e287d9895064cef479002008d087e9d82129f
Linus Liang <linus.h.liang@gmail.com>
2015-09-16 01:53:04 -0400

    production logs changed to info

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,8 +51,9 @@ Rails.application.configure do
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
+  # Set to :info 
   # Set to :debug to see everything in the log.
-  config.log_level = :debug
+  config.log_level = :info
 
   # Prepend all log lines with the following tags.
   # config.log_tags = [ :subdomain, :uuid ]
